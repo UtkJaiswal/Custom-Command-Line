f3e8bb125489ed5b7230bef4b1791912d46c635c
utkarsh <utkjaiswal58@gmail.com>
2024-04-10 15:50:07 +0530

    Dx improved colors added to console messages using chalk version 4

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,27 +1,24 @@
 #!/usr/bin/env node
-
 const arg = require('arg');
+const chalk = require('chalk');
 
 try {
-    const args = arg({
-        '--start' : Boolean,
-        '--build'   : Boolean,
-    })
+  const args = arg({
+    '--start': Boolean,
+    '--build': Boolean,
+  });
 
-    if(args['--start']){
-        console.log("starting the app")
-    }
+  if (args['--start']) {
+    console.log(chalk.bgCyanBright('starting the app'));
+  }
+} catch (e) {
+  console.log(chalk.yellow(e.message));
+  console.log();
+  usage();
 }
 
-catch(e) {
-    console.log(e.message)
-    console.log()
-    usage()
-}
-
-
-function usage(){
-    console.log(`tool [CMD]
-    --start\tStarts the app
-    --build\tBuilds the app`)
+function usage() {
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app`);
 }
