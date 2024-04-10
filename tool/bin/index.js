2d9149038f8481177599ccaeeb155db2cfb6aa44
utkarsh <utkjaiswal58@gmail.com>
2024-04-10 15:32:32 +0530

    basic custom cli done with proper error handling

---
+++ tool/bin/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const arg = require('arg');
+
+try {
+    const args = arg({
+        '--start' : Boolean,
+        '--build'   : Boolean,
+    })
+
+    if(args['--start']){
+        console.log("starting the app")
+    }
+}
+
+catch(e) {
+    console.log(e.message)
+    console.log()
+    usage()
+}
+
+
+function usage(){
+    console.log(`tool [CMD]
+    --start\tStarts the app
+    --build\tBuilds the app`)
+}
