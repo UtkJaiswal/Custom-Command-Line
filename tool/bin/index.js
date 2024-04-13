cfc00872ce69383a4bb088f46e1b3f10a8d53420
utkarsh <utkjaiswal58@gmail.com>
2024-04-13 11:26:02 +0530

    config done without package.json

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const arg = require('arg');
 const chalk = require('chalk');
+const getConfig = require('../src/config/config-mgr');
+const start = require('../src/commands/start');
 
 try {
   const args = arg({
@@ -9,7 +11,8 @@ try {
   });
 
   if (args['--start']) {
-    console.log(chalk.bgCyanBright('starting the app'));
+    const config = getConfig();
+    start(config);
   }
 } catch (e) {
   console.log(chalk.yellow(e.message));
